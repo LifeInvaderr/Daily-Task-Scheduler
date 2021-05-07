27b4ace4fb244c6d74851d61f4a788de1fcade78
LifeInvader <matthewkausfox@hotmail.com>
2021-05-06 22:42:32 -0600

    Time block initial code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,47 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-// Current day is displayed at the top 
-var time = moment().format('dddd MMM Do');
-$("#currentDay").text(time);
-
-
-// Color coded time blocks
+    // Current day is displayed at the top 
+    var time = moment().format('dddd MMM Do');
+    $("#currentDay").text(time);
 
 
+    // Color coded time blocks
+    if (moment().isBefore(moment({ hour: 9, minute:0}))) {
+        $("#9").children("textarea").addClass("bg-success")
+        $("#9").children("textarea").removeClass("bg-danger")
+        $("#9").children("textarea").removeClass("bg-secondary")
+    };
+    if (moment().isAfter(moment({ hour: 9, minute:0}))) {
+        $("#9").children("textarea").addClass("bg-danger")
+        $("#9").children("textarea").removeClass("bg-success")
+        $("#9").children("textarea").removeClass("bg-secondary")
+    };
+    if (moment().isAfter(moment({ hour: 10, minute:0}))) {
+        $("#9").children("textarea").addClass("bg-secondary")
+        $("#9").children("textarea").removeClass("bg-danger")
+        $("#9").children("textarea").removeClass("bg-success")
+    };
 
+    // Save button to local storage
+    $(".saveBtn").on("click", function () {
+        // console.log($(this).parent().attr("id"))
+        var hour = $(this).parent().attr("id")
+        var content = $(this).siblings("textarea").val()
+        // console.log($(this).siblings("textarea").val())
 
-// Save button to local storage
-$(".saveBtn").on("click", function(){
-    // console.log($(this).parent().attr("id"))
-    var hour = $(this).parent().attr("id")
-    var content = $(this).siblings("textarea").val()
-    // console.log($(this).siblings("textarea").val())
+        localStorage.setItem(hour, JSON.stringify(content))
+    })
 
-    localStorage.setItem(hour, JSON.stringify(content))
-})
-
-// Localstorage keys
-$("#9").children("textarea").val(JSON.parse(localStorage.getItem("9")))
-$("#10").children("textarea").val(JSON.parse(localStorage.getItem("10")))
-$("#11").children("textarea").val(JSON.parse(localStorage.getItem("11")))
-$("#12").children("textarea").val(JSON.parse(localStorage.getItem("12")))
-$("#1").children("textarea").val(JSON.parse(localStorage.getItem("1")))
-$("#2").children("textarea").val(JSON.parse(localStorage.getItem("2")))
-$("#3").children("textarea").val(JSON.parse(localStorage.getItem("3")))
-$("#4").children("textarea").val(JSON.parse(localStorage.getItem("4")))
-$("#5").children("textarea").val(JSON.parse(localStorage.getItem("5")))
+    // Localstorage keys
+    $("#9").children("textarea").val(JSON.parse(localStorage.getItem("9")))
+    $("#10").children("textarea").val(JSON.parse(localStorage.getItem("10")))
+    $("#11").children("textarea").val(JSON.parse(localStorage.getItem("11")))
+    $("#12").children("textarea").val(JSON.parse(localStorage.getItem("12")))
+    $("#1").children("textarea").val(JSON.parse(localStorage.getItem("1")))
+    $("#2").children("textarea").val(JSON.parse(localStorage.getItem("2")))
+    $("#3").children("textarea").val(JSON.parse(localStorage.getItem("3")))
+    $("#4").children("textarea").val(JSON.parse(localStorage.getItem("4")))
+    $("#5").children("textarea").val(JSON.parse(localStorage.getItem("5")))
 
 })
 
